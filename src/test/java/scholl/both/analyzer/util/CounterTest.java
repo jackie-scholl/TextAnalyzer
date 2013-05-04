20c0558d04a60adbdd587480c4aaa993206298a6
Jackson <shades97@gmail.com>
2013-05-04 19:49:16 -0400

    Added test folder, JUnit, Hamcrest

---
+++ src/test/java/scholl/both/analyzer/util/CounterTest.java
@@ -0,0 +1,42 @@
+package scholl.both.analyzer.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Class to test the class {@link Counter}.
+ * 
+ * @author Jackson
+ * 
+ */
+@RunWith(JUnit4.class)
+public class CounterTest {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    @Test
+    public void simpleTest() {
+        Counter<String> cntr = new Counter<String>();
+        assertEquals(0, cntr.get("a"));
+        cntr.add("a");
+        assertEquals(1, cntr.get("a"));
+        
+        cntr.add("a");
+        assertEquals(2, cntr.get("a"));
+        
+        cntr.add("a", 2);
+        assertEquals(4, cntr.get("a"));
+        
+        cntr.add("a", -2);
+        assertEquals(2, cntr.get("a"));
+    }
+}
