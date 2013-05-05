9aa84122ffed3a09636f711af6a916dad279a93b
Keller Scholl <keller.scholl@gmail.com>
2013-05-05 19:03:16 -0400

    Began real work on TextAnalyzer

--- src/main/java/scholl/both/analyzer/text/TextAnalyzer.java
+++ src/main/java/scholl/both/analyzer/text/TextAnalyzer.java
@@ -1,8 +1,18 @@
 package scholl.both.analyzer.text;
 
 import java.util.*;
+import scholl.both.analyzer.social.PostSet;
+import scholl.both.analyzer.social.SocialPost;
+import scholl.both.analyzer.text.Text;
 
 public class TextAnalyzer {
+    SocialPost[] posts;
+    /*
+     * Main method is a basic test if given no arguments
+     * If it is given arguments, it returns the text along 
+     * with the number of words and the number of characters in each.
+     * @param some number of strings to analyze
+     */
     public static void main(String[] args) {
         if (args.length == 0) {
             String[] newArgs = { "hello", "hello world", "this is a simple test" };
@@ -14,6 +24,12 @@ public class TextAnalyzer {
             System.out.printf("%s: %d words, %d characters %n", t.getOriginal(), t.getWordCount(),
                     t.getCharacterCount());
         }     
+    }
+    /*
+     * Makes an instance of TextAnalyzer with the given posts.
+     */
+    TextAnalyzer(PostSet p){
+        posts = p.getPosts();
     }
     
 }
