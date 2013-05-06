74d22d6eb8c3c1b95fdbe0dd5f568b585114c028
Jackson <shades97@gmail.com>
2013-05-06 17:35:01 -0400

    Added SocialStatsTest, dependencies to pom.xml

---
+++ src/test/java/scholl/both/analyzer/social/SocialStatsTest.java
@@ -0,0 +1,23 @@
+package scholl.both.analyzer.social;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Class to test the class {@link Counter}.
+ * 
+ * @author Jackson
+ * 
+ */
+@RunWith(JUnit4.class)
+public class SocialStatsTest {
+    
+    @Test
+    public void simpleTest() {
+        SocialStats.tumlbrThing();
+    }
+}
