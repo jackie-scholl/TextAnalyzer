21abceeb758a1a1faea73f29b6f9c756d3e34e42
Jackson <shades97@gmail.com>
2013-05-09 14:54:26 -0400

    Cleaned up, removed dead code

--- src/main/java/scholl/both/analyzer/social/networks/TwitterClient.java
+++ src/main/java/scholl/both/analyzer/social/networks/TwitterClient.java
@@ -18,21 +18,9 @@ import java.util.Set;
 public class TwitterClient implements SocialClient {
     private Twitter twitter;
     
-    public TwitterClient(String fileName) throws IOException {
-        //BufferedReader br = new BufferedReader(new FileReader(fileName));
-        //String consumerKey = br.readLine();
-        //String consumerSecret = br.readLine();
-        //String accessToken = br.readLine();
-        //String tokenSecret = br.readLine();
-        //br.close();
-        
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        
+    public TwitterClient(String fileName) throws IOException {        
+        ConfigurationBuilder cb = new ConfigurationBuilder();        
         cb.setDebugEnabled(true);
-                //.setOAuthConsumerKey(consumerKey)
-                //.setOAuthConsumerSecret(consumerSecret)
-                //.setOAuthAccessToken(accessToken)
-                //.setOAuthAccessTokenSecret(tokenSecret);
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
     }
@@ -175,59 +163,10 @@ public class TwitterClient implements SocialClient {
 
         private TwitterClient getOuterType() {
             return TwitterClient.this;
-        }
-
-        
-        
+        }        
     }
     
     private SocialPost getSocial(Status s) {
         return new SocialPost(s.getText(), new TwitterUser(s.getUser()), s.getCreatedAt().getTime());
     }
-    
-    /*private class TwitterPost extends SocialPost {
-        
-    }*/
 }
-
-
-
-/*public static void main(String[] args) throws IOException, TwitterException {
-    System.out.println("Hello World!");
-    
-    /*BufferedReader br = new BufferedReader(new FileReader("twitter_credentials.txt"));
-    String consumerKey = br.readLine();
-    String consumerSecret = br.readLine();
-    String accessToken = br.readLine();
-    String tokenSecret = br.readLine();
-    br.close();
-    
-    ConfigurationBuilder cb = new ConfigurationBuilder();
-    cb.setDebugEnabled(true)
-            .setOAuthConsumerKey(consumerKey)
-            .setOAuthConsumerSecret(consumerSecret)
-            .setOAuthAccessToken(accessToken)
-            .setOAuthAccessTokenSecret(tokenSecret);
-    TwitterFactory tf = new TwitterFactory(cb.build());
-    Twitter twitter = tf.getInstance();*
-    
-    TwitterClient c = new TwitterClient("twitter_credentials.txt");
-    
-    /*
-    User user = twitter.verifyCredentials();
-    List<Status> statuses = twitter.getHomeTimeline();
-    System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
-    for (Status status : statuses) {
-        System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-    }
-    
-    long cursor = -1;
-    List<User> following = new ArrayList<User>();
-    while (cursor != 0) {
-        PagableResponseList<User> l = twitter.getFriendsList(user.getId(), cursor);
-        following.addAll(l);
-        cursor = l.getNextCursor();
-    }
-    System.out.println(following);
-    *
-}*/
