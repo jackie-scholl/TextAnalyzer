1975b8b4ec3ebc5ead23f636afe047354b104509
Jackson <shades97@gmail.com>
2013-05-09 21:44:46 -0300

    Added equals Theory to CounterTest

--- src/test/java/scholl/both/analyzer/util/CounterTest.java
+++ src/test/java/scholl/both/analyzer/util/CounterTest.java
@@ -14,10 +14,20 @@ import org.junit.runners.JUnit4;
  * 
  */
 @RunWith(JUnit4.class)
+@RunWith(Theories.class)
 public class CounterTest {
+    @DataPoint
+    public static Counter<Byte> blankCounter = new Counter();
+    @DataPoint
+    public static Counter<Byte> nullCounter = null;
+    
+    
+    private Counter<Byte> counter;
+    
+    
     
     @Test
-    public void simpleTest() {
+    public static void simpleTest() {
         Counter<String> cntr = new Counter<String>();
         assertEquals(0, cntr.get("a"));
         cntr.add("a");
@@ -31,5 +41,17 @@ public class CounterTest {
         
         cntr.add("a", -2);
         assertEquals(2, cntr.get("a"));
+    }
+    
+    @Theory
+    public void equals(Counter<Byte> a, Counter<Byte> b) {
+        assumeThat(a != null && b != null);
+        boolean equal = true;
+        for (byte i = 0; i > -1; i++) {
+            equal &= a.get(b) == b.get(a);
+            equal &= a.contains(b) == a.contains(b);
+        }
+        assertEqual(equal, a.equals(b));
+        assertEqual(equal, b.equals(a));
     }
 }
