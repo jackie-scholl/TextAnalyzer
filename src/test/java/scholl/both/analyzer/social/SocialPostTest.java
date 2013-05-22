90fd3159259f0774c2270d49e0a93efa9b2254fa
Jackson <shades97@gmail.com>
2013-05-22 13:59:13 -0400

    Moved SocialPostTest

--- src/test/java/scholl/both/analyzer/social/SocialPostTest.java
+++ src/test/java/scholl/both/analyzer/social/SocialPostTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -25,10 +26,12 @@ public class SocialPostTest {
     
     @DataPoint public static SocialUser nullUser = null;
     @DataPoint public static SocialUser mockUserJackson = new MockSocialUser("Jackson");
+    @DataPoint public static SocialUser mockUserKeller = new MockSocialUser("Keller");
     
     @DataPoint public static List<String> nullStrList = null;
     @DataPoint public static List<String> emptryStrList = new ArrayList<String>();
-    @DataPoint public static List<String> sampleStrList = Arrays.asList(new String[]{"a", "b", "c", "d"});
+    @DataPoint public static List<String> sampleStrList = Arrays.asList(new String[]{"a", "b", "b", "d"});
+    @DataPoint public static List<String> sampleStrList2 = Arrays.asList(new String[]{"e", "c", "g", "h"});
     
     @BeforeClass
     public static void setupClass() {
@@ -51,18 +54,49 @@ public class SocialPostTest {
     }
     
     @Theory
-    public void constructorTest(String str, long millis, SocialUser poster) {
+    public void constructorTest(String str) {
         assumeThat(str, is(notNullValue()));
-        SocialPost a = new SocialPost(str, poster, millis);
-        SocialPost b = new SocialPost(poster, millis, str, null, new ArrayList<String>());
+        SocialPost a = new SocialPost(str);
+        SocialPost b = new SocialPost(str, a.getTimestamp(), null);
         assertThat(a, is(equalTo(b)));
     }
     
     @Theory
-    public void getPosterTest(String str, long millis, SocialUser poster) {
+    public void constructorTest2(String str, long millis, SocialUser poster) {
+        assumeThat(str, is(notNullValue()));
+        SocialPost a = new SocialPost(str, millis, poster);
+        SocialPost b = new SocialPost(str, millis, poster, null, new ArrayList<String>());
+        assertThat(a, is(equalTo(b)));
+    }
+        
+    @Theory
+    public void simpleConstructorTheory(String str, long millis, SocialUser poster, 
+            SocialUser mention, List<String> tags) {
         assumeThat(str, is(notNullValue()));
         assumeThat(poster, is(notNullValue()));
-        SocialPost a = new SocialPost(str, poster, millis);
+        assumeThat(mention, is(notNullValue()));
+        assumeThat(tags, is(notNullValue()));
+        
+        SocialPost a = new SocialPost(str, millis, poster, mention, tags);
+        assertThat(a.getOriginal(), is(equalTo(str)));
+        assertThat(a.getTimestamp(), is(equalTo(millis)));
         assertThat(a.getPoster(), is(equalTo(poster)));
+        assertThat(a.getMention(), is(equalTo(mention)));
+        assertThat(a.getTags(), is(equalTo(tags)));
+    }
+    
+    @Theory
+    public void nullTagsTheory(String str, long millis, SocialUser poster, SocialUser mention) {
+        assumeThat(str, is(notNullValue()));
+        
+        Exception ex = null;
+        
+        try {
+            new SocialPost(str, millis, poster, mention, null);
+        } catch (NullPointerException e) {
+            ex = e;
+        }
+        
+        assertThat(ex, is(notNullValue()));
     }
 }
